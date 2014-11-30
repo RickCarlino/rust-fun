7bf904b651c64ec1decc4b4fd07e00e0b9affdd6
Rick Carlino <rick.carlino@gmail.com>
2014-11-29 21:38:01 -0600

    Wow. That was nuts

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@ extern crate redis;
 use redis::Commands;
 
 fn main() {
-    println!("Hello, world!")
+    let lol = fetch_an_integer();
+    match lol {
+      Ok(result) => println!("The number is {}", result),
+      Err(abc) => println!("ERROR!!! {}", abc)
+    }
 }
 
-// fn fetch_an_integer() -> redis::RedisResult<int> {
-//     // connect to redis
-//     let client = try!(redis::Client::open("redis://127.0.0.1/"));
-//     let con = try!(client.get_connection());
-//     // throw away the result, just make sure it does not fail
-//     let _ : () = try!(con.set("my_key", 42i));
-//     // read back the key and return it.  Because the return value
-//     // from the function is a result for integer this will automatically
-//     // convert into one.
-//     con.get("my_key")
-// }
+fn fetch_an_integer() -> redis::RedisResult<int> {
+    // connect to redis
+    let client = try!(redis::Client::open("redis://127.0.0.1/"));
+    let con = try!(client.get_connection());
+    // throw away the result, just make sure it does not fail
+    let _ : () = try!(con.set("my_key", 42i));
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    con.get("my_key")
+}
